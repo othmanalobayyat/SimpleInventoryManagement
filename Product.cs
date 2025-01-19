0759d327d5b54e389ff6e7b77c1804083c7e833c
Othman <othmanalobayyat@gmail.com>
2025-01-20 01:56:19 +0200

    Add  product class

---
+++ Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryManagement
+{
+    public class Product
+    {
+        private string name;
+        private double price;
+        private int quantity;
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
